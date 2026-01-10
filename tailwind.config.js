8f82b08ca7663baca0133369cc32abdcd1686166
Ahmadmeltaha <ahmadbalaawi.ab@gmail.com>
2026-01-10 21:02:16 +0300

    Add config files and package-lock.json

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#1d4ed8',
+        dark: '#0f172a',
+      },
+    },
+    fontFamily: {
+      sans: ['Atkinson Hyperlegible', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
